3daa3585ed680efd20dcd8bb0fa725a7982481e5
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-07-10 12:43:13 -0700

    initiate a new console application in C# .NET.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("What is your name?");
+            Console.ReadKey();
+        }
+    }
+}
