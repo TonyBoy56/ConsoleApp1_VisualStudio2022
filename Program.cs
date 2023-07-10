151b9466dcc89a59dab91349e71f30af0ddb3844
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-07-10 15:22:05 -0700

    add Console statements to show ascii value of string characters.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
             Console.WriteLine("What is your name?");
+            int asciicode = Console.Read();
+            Console.WriteLine(asciicode);
             Console.ReadKey();
+
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Dear {name}! Welcome to our lesson");
+            //Console.ReadKey();
         }
     }
 }
